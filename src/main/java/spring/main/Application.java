7f66adc7c80bfd3d358ad36161ea284246f3112c
Bissuelquentin <qbissuel@gmail.com>
2019-12-06 15:18:11 +0100

    Cleaning Application.java

--- src/main/java/spring/main/Application.java
+++ src/main/java/spring/main/Application.java
@@ -1,11 +1,17 @@
 package spring.main;
 
+import org.apache.jena.rdfconnection.RDFConnection;
+import org.apache.jena.rdfconnection.RDFConnectionFactory;
+import org.apache.jena.update.UpdateExecutionFactory;
+import org.apache.jena.update.UpdateFactory;
+import org.apache.jena.update.UpdateProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
