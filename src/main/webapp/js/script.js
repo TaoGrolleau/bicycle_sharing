dec29ee00edd68772e4aa2466df06a20191daa91
Bissuelquentin <qbissuel@gmail.com>
2019-12-09 16:18:46 +0100

    Add clickable marker on map

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -26,16 +26,32 @@ function initMap() {
         maxZoom: 20
     }).addTo(macarte);
 
-    for (ville in villes) {
-        var marker = L.marker([villes[ville].lat, villes[ville].lon]).addTo(macarte);
+    for (station in listStationName) {
+        var marker = L.marker([parseFloat(listStationLat[station]), parseFloat(listStationLon[station])] ,{
+                                                                                                              id: listId[station],
+                                                                                                              title: listStationName[station],
+                                                                                                              someCustomProperty: 'Adding custom data to this marker!',
+                                                                                                              anotherCustomProperty: 'More custom data to this marker!'
+                                                                                                          });
+        marker.addTo(macarte);
         markers.push(marker);
     }
 
     var group = new L.featureGroup(markers); // Nous créons le groupe des marqueurs pour adapter le zoom
-    macarte.fitBounds(group.getBounds().pad(0.1)); // Nous demandons à ce que tous les marqueurs soient visibles, et ajoutons un padding (pad(0.5)) pour que les marqueurs ne soient pas coupés
+    group.on('click',onClick);
+    macarte.fitBounds(group.getBounds().pad(0.1));// Nous demandons à ce que tous les marqueurs soient visibles, et ajoutons un padding (pad(0.5)) pour que les marqueurs ne soient pas coupés
     macarte.addLayer(markerClusters);
 }
-    window.onload = function(){
+
+window.onload = function(){
     // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
     initMap();
 };
+
+function onClick(e){
+    var marker = e.layer;
+    location.replace("http://localhost:8080/station?id=" + marker.options.id);
+}
+
+
+
